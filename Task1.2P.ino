8ae0c7fcb32f10fcdaa2d147f5b7b15071534c21
Harshitasanan20 <137972096+Harshitasanan20@users.noreply.github.com>
2024-01-31 19:24:46 +0530

    Update Task1.2P.ino

--- Task1.2P.ino
+++ Task1.2P.ino
@@ -5,7 +5,7 @@ int tempt = 0;
 void setup() {
   pinMode(intPin, INPUT_PULLUP);
   pinMode(4, OUTPUT);
-  pinMode(sensorPin, INPUT);  // initialize sensor as an input
+  pinMode(sensorPin, INPUT);  
   Serial.begin(9600);         
   attachInterrupt(digitalPinToInterrupt(intPin), temp, RISING);
 }
@@ -13,24 +13,16 @@ void setup() {
 void loop() {
   int sensorValue = analogRead(sensorPin);
 
-  // Convert the analog value to temperature in degrees Celsius
   float voltage = sensorValue * (5.0 / 1023.0);
   float temperatureCelsius = (voltage - 0.5) * 100.0;
 
   
-  Serial.print("Current Temperature: ");
-  Serial.print(temperatureCelsius);
-  Serial.println(" degrees Celsius");
-
-  
-  if (temperatureCelsius > 25) {  
+  if (temperatureCelsius > 50) {  
     digitalWrite(4, HIGH);
     digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("LED turned ON");
   } else {
     digitalWrite(4, LOW);
     digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("LED turned OFF");
   }
   delay(1000);
 }
